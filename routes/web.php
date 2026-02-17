a92cf795035902d6f7a5a00c64bde25e612d7e3e
Isabel Anez <maranemen@alu.edu.gva.es>
2026-02-17 15:07:49 +0100

    Creamos las rutas y el controlador para Category

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
@@ -8,3 +9,4 @@ Route::get('/', function () {
 });
 
 Route::resource('posts', PostController::class);
+Route::resource('categories', CategoryController::class);
